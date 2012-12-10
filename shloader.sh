8fb43ae288ad65e6bedefc38f6e1ce96d832800c
shloaderdev <lacuspam@gmail.com>
2012-12-09 21:33:07 -0600

    replaced 'scala' with 'java' in shloader.sh

--- shloader.sh
+++ shloader.sh
@@ -2,12 +2,12 @@
 
 while true
     do
-        scala -cp "target/shloader-1.0-SNAPSHOT.jar:target/lib/*" com.shloader.Shloader
+        java -cp "target/shloader-1.0-SNAPSHOT.jar:target/lib/*" com.shloader.Shloader
 
         OUT=$?
         if [ $OUT -ne 0 ];then
            echo "Shloader failed. Exit code: $OUT"
-           exit 0
+           #exit 0
         fi
 
         echo "sleeping..."
