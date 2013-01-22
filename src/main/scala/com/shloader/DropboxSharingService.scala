16a0aec5d3fa6b4f558d81c4df58aff7e2c4b95e
shloaderdev <lacuspam@gmail.com>
2013-01-21 22:06:33 -0600

    replace system properties in dropbox service

--- src/main/scala/com/shloader/DropboxSharingService.scala
+++ src/main/scala/com/shloader/DropboxSharingService.scala
@@ -9,7 +9,7 @@ import java.io.File
 class DropboxSharingService extends SharingService {
   override def share() {
     val source = new File(Config.getAndReplaceSystemProperties("torrent.client.dir.completed"))
-    val dest = new File(Config.getOption("dropbox.dir").getOrElse(Defaults.DROPBOX_DIR))
+    val dest = new File(Config.getAndReplaceSystemProperties("dropbox.dir").getOrElse(Defaults.DROPBOX_DIR))
     FileUtils.copyDirectory(source, dest)
     FileUtils.cleanDirectory(source)
   }
